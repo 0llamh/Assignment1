affcbb0f0ac5e918386a56d8ba3472575343bfdd
Cameron Smith <smith.paulcameron@gmail.com>
2017-10-12 15:17:00 -0400

    Fixed bugs

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -8,7 +8,7 @@
 
 double CrazyRandomSword::hit(double armor) {
     double damage = (rand() % 90) + hitPoints; 	// hitPoints = 10; random damage between 10 and 100
-    damage -= rand() % ((int)armor / 2);	// decreast damage by a random number between 0 and half the armor
+    damage -= rand() % (int)(armor / 2.0);	// decreast damage by a random number between 0 and half the armor
 
     if (damage < 0) {
         return 0;
